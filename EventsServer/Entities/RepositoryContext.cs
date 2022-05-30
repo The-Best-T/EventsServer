13328dece7d6dbb739aa74ed2da9f777611e4b7f
The_Best_T <zhamichenok@gmail.com>
2022-05-30 18:04:04 +0300

    add context

---
+++ EventsServer/Entities/RepositoryContext.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities
+{
+    public class RepositoryContext : DbContext
+    {
+        private DbSet<Event> Events { get; set; } = null!;
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
