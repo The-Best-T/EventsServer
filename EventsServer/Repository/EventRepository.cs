b50eeaf5e34ceb4d71c96b3478fc57a05a0fffc0
The_Best_T <zhamichenok@gmail.com>
2022-05-30 18:49:20 +0300

    add event repository

---
+++ EventsServer/Repository/EventRepository.cs
@@ -0,0 +1,13 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+
+namespace Repository
+{
+    public class EventRepository : RepositoryBase<Event>, IEventRepository
+    {
+        public EventRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+    }
+}
